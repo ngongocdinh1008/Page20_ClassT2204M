78981d5d65873b5d71d0f52ca4b9fd0f4ec870d9
JRosé <89382734+ngongocdinh1008@users.noreply.github.com>
2024-02-26 10:29:39 +0700

    Delete ManagerContact directory

--- ManagerContact/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ManagerContact.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
